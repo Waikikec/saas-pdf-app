1378b277bd79ae4dd792a4cabc73f601d0c345a4
Waikikec <martin.d.yankov@gmail.com>
2023-12-07 15:50:58 +0200

    feat: displaying messages

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -3,6 +3,7 @@ import { trpc } from '@/app/_trpc/client';
 import { ChevronLeft, Loader2, XCircle } from 'lucide-react';
 import Link from 'next/link';
 import { buttonVariants } from '../ui/button';
+import { ChatContextProvider } from './ChatContext';
 import ChatInput from './ChatInput';
 import Messages from './Messages';
 
@@ -85,13 +86,15 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     );
 
   return (
-    <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
-      <div className='flex-1 justify-between flex flfex-col mb-28'>
-        <Messages />
-      </div>
+    <ChatContextProvider fileId={fileId}>
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 justify-between flex flfex-col mb-28'>
+          <Messages fileId={fileId} />
+        </div>
 
-      <ChatInput />
-    </div>
+        <ChatInput />
+      </div>
+    </ChatContextProvider>
   );
 };
 
